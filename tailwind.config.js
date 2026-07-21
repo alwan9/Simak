5714c39c94fa8eba948365c44dd77ded06e266fd
alwan9 <hafizsusilo86@gmail.com>
2026-07-21 08:03:35 +0700

    perbaikan layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,15 @@ module.exports = {
 
   ],
   theme: {
-    screens: {
-      xs1: "270px",
-      sm1: "400px",
-      md1: "620px",
-      lg1: "776px",
-      xl1: "1240px",
+    extend: {
+      screens: {
+        xs1: "270px",
+        sm1: "400px",
+        md1: "620px",
+        lg1: "776px",
+        xl1: "1240px",
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
